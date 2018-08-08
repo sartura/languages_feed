3e25272eee115df80183c1a021e7b015f7b4427d
Mislav Novakovic <mislav.novakovic@sartura.hr>
2018-08-08 12:24:30 +0200

    add hello-world-C example
    Signed-off-by: Mislav Novakovic <mislav.novakovic@sartura.hr>

---
+++ hello-world-C/files/src/main.c
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2018 Sartura Ltd.
+ *
+ * Permission is granted to copy, distribute and/or modify this document
+ * under the terms of the GNU Free Documentation License, Version 1.2
+ * or any later version published by the Free Software Foundation;
+ * with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+ * Texts.  A copy of the license is included in the section entitled "GNU
+ * Free Documentation License".
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <err.h>
+
+char response[] = "HTTP/1.1 200 OK\r\n"
+"Content-Type: text/html; charset=UTF-8\r\n\r\n"
+"<!DOCTYPE html><html><head><title>hello world</title>"
+"<body><h1>Hello world!</h1></body></html>\r\n";
+
+int main()
+{
+    int one = 1, client_fd;
+    struct sockaddr_in svr_addr, cli_addr;
+    socklen_t sin_len = sizeof(cli_addr);
+
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+        err(1, "can't open socket");
+
+    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
+
+    int port = 8000;
+    svr_addr.sin_family = AF_INET;
+    svr_addr.sin_addr.s_addr = INADDR_ANY;
+    svr_addr.sin_port = htons(port);
+
+    if (bind(sock, (struct sockaddr *) &svr_addr, sizeof(svr_addr)) == -1) {
+        close(sock);
+        err(1, "Can't bind");
+    }
+
+    listen(sock, 5);
+    while (1) {
+        client_fd = accept(sock, (struct sockaddr *) &cli_addr, &sin_len);
+        printf("got connection\n");
+
+        if (client_fd == -1) {
+            perror("Can't accept");
+            continue;
+        }
+
+        write(client_fd, response, sizeof(response) - 1); /*-1:'\0'*/
+        close(client_fd);
+    }
+}
