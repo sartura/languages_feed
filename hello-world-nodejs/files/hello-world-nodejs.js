0cbc7a76641055a63c0b79aa20c1e69823e5e38c
Mislav Novakovic <mislav.novakovic@sartura.hr>
2018-10-25 14:35:26 +0200

    add hello-world-nodejs example
    Signed-off-by: Mislav Novakovic <mislav.novakovic@sartura.hr>

---
+++ hello-world-nodejs/files/hello-world-nodejs.js
@@ -0,0 +1,16 @@
+//
+// Copyright (C) 2018 Sartura Ltd.
+//
+// Permission is granted to copy, distribute and/or modify this document
+// under the terms of the GNU Free Documentation License, Version 1.2
+// or any later version published by the Free Software Foundation;
+// with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+// Texts.  A copy of the license is included in the section entitled "GNU
+// Free Documentation License".
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello world!');
+}).listen(8010, '0.0.0.0');
